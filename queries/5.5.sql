10fcbb97163842513bef518c3fb92511f8c005c7
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-12 17:37:53 +0100

    refactor

--- queries/5.5.sql
+++ queries/5.5.sql
@@ -1,8 +1,9 @@
-# 5. Nombre de los constructores con los que han disputado
+# 5.5 Nombre de los constructores con los que han disputado
 # carreras más de 50 pilotos diferentes.
 # (Mostrando también el número de pilotos)
 
 SELECT c.name, COUNT(DISTINCT re.driverId) AS pilotos
-FROM constructors c JOIN results re ON c.constructorId = re.constructorId
+FROM constructors c
+         JOIN results re ON c.constructorId = re.constructorId
 GROUP BY c.constructorId
 HAVING pilotos > 50;
