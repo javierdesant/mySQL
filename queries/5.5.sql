7984cbe6413095de8464cab5ca1de58e02721e1e
Javier de Santiago <j.desantiagosoto@gmail.com>
2024-11-12 12:47:29 +0100

    feat: add query 5.5

---
+++ queries/5.5.sql
@@ -0,0 +1,8 @@
+# 5. Nombre de los constructores con los que han disputado
+# carreras más de 50 pilotos diferentes.
+# (Mostrando también el número de pilotos)
+
+SELECT c.name, COUNT(DISTINCT re.driverId) AS pilotos
+FROM constructors c JOIN results re ON c.constructorId = re.constructorId
+GROUP BY c.constructorId
+HAVING pilotos > 50;
